132126376d514ea9eb7fae385e7f87ddacbc8845
unknown <mahdi.roshanzamir.1376@gmail.com>
2022-05-12 12:30:21 +0430

    setup template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap'
+import { BiEditAlt } from 'react-icons/bi';
+import { AiOutlineDelete } from 'react-icons/ai';
+import './App.css'
 
 function App() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-12 d-flex justify-content-between">
+            <h5 class="card-title m-b-0">User list</h5>
+            <button className="btn btn-primary" onClick={handleShow}>Add user</button>
+            <Modal show={show} onHide={handleClose} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>Add user form</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="row">
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Family</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Username</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Birthday</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Gender</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                      <Form.Label>Role</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Add
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+          <div className="col-12 mt-3">
+            <div class="card">
+              <div class="table-responsive">
+                <table class="table">
+                  <thead class="thead-light">
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Family</th>
+                      <th scope="col">Username</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Birthday</th>
+                      <th scope="col">Gender</th>
+                      <th scope="col">Role</th>
+                      <th scope="col" className="text-center">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody class="customtable">
+                    <tr>
+                      <td>mahdi</td>
+                      <td>Roshanzamir</td>
+                      <td>mahdi-roshan</td>
+                      <td>mahdi@gmail.com</td>
+                      <td>21/8/1997</td>
+                      <td>male</td>
+                      <td>Admin</td>
+                      <td className="text-center">
+                        <BiEditAlt className="user-edit-icon" />
+                        <AiOutlineDelete className="user-delete-icon" />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
